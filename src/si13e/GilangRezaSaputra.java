1d2472f66673371c469286a18bdd4f686103c963
gilangrezasaputra <gilangreza944@yahoo.co.id>
2014-10-07 08:23:41 +0700

    versi 2.3.2

---
+++ src/si13e/GilangRezaSaputra.java
@@ -0,0 +1,26 @@
+
+
+package si13e;
+
+
+public class GilangRezaSaputra {
+
+   
+    public static void main(String[] args) {
+        
+        System.out.println("teknokrat adalah sebuah universitas yg menggambarkan sebuah wadah pembelajaran untuk kita semua");
+        String nama   = ("Gilang Reza Saputra");
+        String kelas  = ("SI13E");
+        String npm    = ("13311054");
+        String UKM    = ("UKM band");
+        String alamat = ("sukabumi,bandarlampung");
+                System.out.println("Nama   = "+nama);
+                System.out.println("Kelas  = "+kelas);
+                System.out.println("Npm    = "+npm);
+                System.out.println("Ukm    = "+UKM);
+                System.out.println("Alamat = "+alamat);
+          System.out.println("tugas upload repositori");
+        
+    }
+    
+}
